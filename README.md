d953b0beb7f5e720a2658f36474c136297ca6c8e
RossDAI <Rainnighttram@proton.me>
2026-05-05 21:59:32 +0800

    Add statistics functions

--- README.md
+++ README.md
@@ -1,17 +1,31 @@
 # VLLM Performance Analyser
 A automated script that extract key performance metrics from vllm serving logs.
 
-# Quick Start
+## Quick Start
 ```
 git clone https://github.com/AnaOnTram/vllm_analyser.git #clone the directory
 cd vllm_analyser
 ```
-Copy your vllm log to a txt file. 
+Copy your vllm log and save it a txt file. 
 ```
 python log_analysis.py #start analyzing
 ```
 
-# Model inference engine support
+## Sample Outputs
+1. basic statistics
+```bash
+Average Decode Speed: 16.272289156626506
+Decode Speed Standard Deviation: 6.470727957935853
+MTP Avarage Acceptance Rate: 40.05802469135803%
+MTP Acceptance Rate Standard Deviation: 14.989561903183919%
+You may consider decrease "num_speculative_tokens" value to increase decode performance.
+```
+2. plot
+<img src='Assets/nvidia_NVIDIA-Nemotron-3-Super-120B-A12B-NVFP4_perf.png'>
+
+3. [sorted csv](Assets/nvidia_NVIDIA-Nemotron-3-Super-120B-A12B-NVFP4_performance.csv)
+
+## Model inference engine support
 - [x] vllm
 - [ ] sglang
 - [ ] llama.cpp
--- log_analysis.py
+++ log_analysis.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
+import csv, statistics
 
-file = open('log.txt', 'r')
 prefill = list()
 decode = list()
 drafts = list()
@@ -16,6 +16,7 @@ try:
             if '[utils.py:233]' in components:
                 sp0 = components.index("{'model_tag':")
                 model_name = ''.join(components[sp0 + 1: sp0 + 2])[1:-2]
+                model_name = model_name.replace('/', '_')
             if '[loggers.py:271]' in components:
                 sp1 = components.index('throughput:')
                 sp2 = components[sp1+1:].index('throughput:')
@@ -31,7 +32,7 @@ try:
                 avg_accept = float(''.join(components[sp4+4:sp4+5])[:4])
                 drafts.append(draft_throughput)
                 acceptance.append(avg_accept)
-                
+    
     plt.plot(range(len(prefill)), prefill, color='green')
     plt.ylabel('Prefill Speed')
     plt.title(f'{model_name} Prefill Performance')
@@ -51,12 +52,37 @@ try:
         plt.show()
     
         plt.plot(range(len(decode)), decode, color='red', label='decode')
-        plt.plot(range(len(drafts)), drafts, color='blue', label='draft', linestyle='-.')
-        plt.plot(range(len(acceptance)), acceptance, color='orange', label='draft', linestyle=':')
+        plt.plot(range(len(drafts)), drafts, color='blue', label='draft_speed', linestyle='-.')
+        plt.plot(range(len(acceptance)), acceptance, color='orange', label='acceptance_rate', linestyle=':')
         plt.ylabel('decode speed')
         plt.legend()
         plt.title(f'{model_name} Speculative Performance')
-        plt.savefig('{model_name}_perf.png', dpi=300)
+        plt.savefig(f'{model_name}_perf.png', dpi=300)
         plt.show()
+
+    if len(acceptance) > 2:
+        new_file = open(f'{model_name}_performance.csv', 'w', newline='')
+        new_writer = csv.DictWriter(new_file, ['Prefill', 'Decode', 'Average Acceptance Rate'])
+        new_writer.writeheader()
+        number = min(len(prefill), len(decode), len(acceptance))
+        for i in range(number):
+            new_writer.writerow({'Prefill': prefill[i], 'Decode': decode[i], 'Average Acceptance Rate': acceptance[i]})
+    else: 
+        new_file = open(f'{model_name}_performance.csv', 'w', newline='')
+        new_writer = csv.DictWriter(new_file, ['Prefill', 'Decode'])
+        new_writer.writeheader()
+        number = min(len(prefill), len(decode))
+        for i in range(number):
+            new_writer.writerow({'Prefill': prefill[i], 'Decode': decode[i]})
+    
+    new_file.close()
+    print(f'Average Decode Speed: {statistics.mean(decode)}')
+    print(f'Decode Speed Standard Deviation: {statistics.stdev(decode)}')
+    if len(acceptance) > 2:
+        print(f'MTP Avarage Acceptance Rate: {statistics.mean(acceptance)}%')
+        print(f'MTP Acceptance Rate Standard Deviation: {statistics.stdev(acceptance)}%')
+        if statistics.mean(acceptance) <= 50:
+            print('You may consider decrease "num_speculative_tokens" value to increase decode performance.')
+
 except:
     print('Input Error! Cannot Find the file. Please check your input and re-run the programme.')
